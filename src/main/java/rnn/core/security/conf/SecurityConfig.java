ed3433951612dcf1328b9932df461e84d413cae4
Ryatronth <roman-novikov-2904@mail.ru>
2024-10-17 22:41:01 +0500

    feat: added logic for authorization with github, gitlab

---
+++ src/main/java/rnn/core/security/conf/SecurityConfig.java
@@ -0,0 +1,28 @@
+package rnn.core.security.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(req -> req
+                        .requestMatchers("/", "/login", "/error", "/test").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
