2e51f9b0cba6babd2f92ac59b8c1b03e3b6dfceb
Ryatronth <roman-novikov-2904@mail.ru>
2024-10-18 00:11:38 +0500

    feat: added cors policy

--- src/main/java/rnn/core/controller/AuthController.java
+++ src/main/java/rnn/core/controller/AuthController.java
@@ -5,18 +5,12 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.Map;
 
 @RestController
 public class AuthController {
     @GetMapping("/user")
     public Map<String, Object> getUser(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return "test";
+        return principal.getAttributes();
     }
 }
--- src/main/java/rnn/core/security/conf/SecurityConfig.java
+++ src/main/java/rnn/core/security/conf/SecurityConfig.java
@@ -1,5 +1,6 @@
 package rnn.core.security.conf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,10 +8,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.default_success_url}")
+    public String defaultSuccessUrl;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -18,11 +28,27 @@ public class SecurityConfig {
                         .requestMatchers("/", "/login", "/error", "/test").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(l -> l
+                        .defaultSuccessUrl(defaultSuccessUrl)
+                )
                 .logout(l -> l
                         .logoutSuccessUrl("/").permitAll()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
